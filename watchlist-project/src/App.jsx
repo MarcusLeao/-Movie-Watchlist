79326c64e96624668b87260c4409b56809fd0d57
MarcusLeão <marcuscarneironunes@gmail.com>
2023-06-11 12:42:37 -0300

    Remove Import of App.css

--- watchlist-project/src/App.jsx
+++ watchlist-project/src/App.jsx
@@ -4,7 +4,6 @@ import { Header } from "./components/Header";
 import { Watchlist } from "./components/Watchlist";
 import { Watched } from "./components/Watched";
 import { Add } from "./components/Add";
-import './App.css'
 import { GlobalProvider } from './context/GlobalState';
 
 import "./lib/font-awesome/css/all.min.css";
@@ -15,15 +14,15 @@ function App() {
   return (
     <GlobalProvider>
       <Router>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path="/" element={<Watchlist/>}  />
-          <Route path="/watched" element={<Watched/>}  />
-          <Route path="/add" element={<Add/>}  />
+          <Route path="/" element={<Watchlist />} />
+          <Route path="/watched" element={<Watched />} />
+          <Route path="/add" element={<Add />} />
         </Routes>
       </Router>
     </GlobalProvider>
-    
+
   )
 }
 
