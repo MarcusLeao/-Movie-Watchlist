9f558651b53c19b66ccdf2b7071ca19f2fc87d07
MarcusLeão <marcuscarneironunes@gmail.com>
2023-06-10 21:05:08 -0300

    Project create and adding components and routes

---
+++ watchlist-project/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Watchlist } from "./components/Watchlist";
+import { Watched } from "./components/Watched";
+import { Add } from "./components/Add";
+import './App.css'
+//import "./lib/font-awesome/css/all.min.css";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+      <Route path="/" element={<Watchlist/>}  />
+      <Route path="/watched" element={<Watched/>}  />
+      <Route path="/add" element={<Add/>}  />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
