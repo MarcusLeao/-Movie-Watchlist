e148a5cdd0c0e86641e37b13549704ada793ca9b
MarcusLeão <marcuscarneironunes@gmail.com>
2023-06-11 00:53:10 -0300

    Creation of header and init Add component and WIP tailwind config

--- watchlist-project/src/components/Add.jsx
+++ watchlist-project/src/components/Add.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 export const Add = () => {
+
+  const [query, setQuery] = useState("");
+  const [results,setResults] = useState([]);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjJlYmJmN2Q3MTZlYWMxZWUzNzg4ZjRjYTdmNzMxYSIsInN1YiI6IjY0ODUxMjQ5ZTM3NWMwMDExYzdmZjZlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OacjGq7Gm8H10Tc-DVm1Xq0GUDkKTds6p8xMwR1RbhI'
+    }
+  };
+
+  const onChange = (e)=>{
+    e.preventDefault();
+
+    setQuery(e.target.value);
+
+    
+
+    const url = `https://api.themoviedb.org/3/search/movie?query=${e.target.value}&include_adult=false&language=pt-BR`;
+
+    fetch(url, options)
+    .then((res)=> res.json())
+    .then((data)=>{
+      if(!data.errors){
+        setResults(data.results)
+      }else{
+        setResults([]);
+      }
+    });
+  }
   return (
-    <div>
-        <h1>Add Page</h1>
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <div className='input-wrapper'>
+            <input 
+              type="text"
+              placeholder="Search for a movie"
+              value={query}
+              onChange={onChange} 
+            />
+          </div>
+        </div>
+      </div>        
     </div>
   )
 }
--- watchlist-project/src/components/Header.jsx
+++ watchlist-project/src/components/Header.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar } from "flowbite-react";
 
 export const Header = () => {
   return (
-    <header>
+    <header className="bg-sky-950 ">
       <div className="container">
-        <div className="inner-content">
-          <div className="brand">
-            <Link to="/">WatchList</Link>
-          </div>
+        <div className="flex justify-between items-center py-2.5">
+        <Navbar.Brand
+        onClick={() => navigate("/")}
+        className="cursor-pointer"
+      >
+        <img
+          src="https://www.freepnglogos.com/uploads/film-reel-png/film-reel-home-feist-electronicsfeist-electronics-22.png"
+          className="mr-3 h-6 sm:h-9"
+          alt="MyWatchlist"
+          />
+        <span className="text-white text-xl no-underline transition-all duration-0 ease-linear hover:opacity-70">
+        MyWatchlist
+        </span>
+      </Navbar.Brand>
 
-          <ul className="nav-links">
-            <li>
-              <Link to="/">Watch List</Link>
+          <ul className="p-0 m-0 list-none flex items-center">
+            <li className="inline-block mr-7 last:mr-0">
+              <Link className="text-white text-xl no-underline transition-all duration-0 ease-linear hover:opacity-70" to="/">Watch List</Link>
             </li>
 
-            <li>
-              <Link to="/watched">Watched</Link>
+            <li className="inline-block mr-7 last:mr-0">
+              <Link className="text-white text-xl no-underline transition-all duration-0 ease-linear hover:opacity-70" to="/watched">Watched</Link>
             </li>
 
-            <li>
-              <Link to="/add" className="btn">
+            <li className="inline-block mr-7 last:mr-0">
+              <Link to="/add" className="px-3.5 py-2.5 bg-lime-500 text-sky-950 rounded-md uppercase font-bold inline-block border-none text-base transition-all duration-0 ease-linear leading-tight hover:opacity-100 hover:bg-lime-700 hover: cursor-pointer disabled:opacity-50 disabled:pointer-events-none">
                 + Add
               </Link>
             </li>
